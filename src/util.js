09d51c644fb49e0bc1cf05af755437802e197bcc
gjethwani <gdjethwa@usc.edu>
2021-01-03 14:03:06 -0500

    Change url

--- src/util.js
+++ src/util.js
@@ -1,15 +1,17 @@
 const getEnv = () => {
     if (window.location.hostname === 'localhost') {
         return 'local'
-    }
-    if (window.location.hostname === 'songq.io' || window.location.hostname === 'www.songq.io') {
+    } else {
         return 'production'
     }
+    // if (window.location.hostname === 'songq.io' || window.location.hostname === 'www.songq.io') {
+    //     return 'production'
+    // }
 }
 
 const getURL = () => {
     if (getEnv() === 'production') {
-        return `${window.location.protocol}//songq-backend.herokuapp.com`
+        return `${window.location.protocol}//api.songq.io`
     } else {
         return `${window.location.protocol}//localhost:5000`
     }
