140abc44333c45e1030c88373bb4568f1e0caac8
gjethwani <gdjethwa@usc.edu>
2021-03-02 21:41:28 -0800

    Fix spacing

--- src/FooterComponent/FooterComponent.js
+++ src/FooterComponent/FooterComponent.js
@@ -1,8 +1,10 @@
 import { Layout } from 'antd'
 import { 
     footer,
-    footerElem
+    footerElem,
+    footerMobile
 } from './Footer.module.css'
+import { isMobile } from 'react-device-detect'
 
 const { Footer } = Layout
 
@@ -10,7 +12,7 @@ const FooterComponent = props => {
     const footerStyle = props.transparentBackground ? { background: 'none' } : {}
     const aStyle = props.transparentBackground ? { color: 'black' } : {}
     return(
-        <Footer className={footer} style={footerStyle}>
+        <Footer className={isMobile ? footerMobile : footer} style={footerStyle}>
             <a 
                 href='/terms-and-conditions.html' 
                 target='_blank' 
